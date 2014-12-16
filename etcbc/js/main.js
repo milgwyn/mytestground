053b6b543a1f4ae7d66f902ad3edc1aea754b14b
Richard Tsang <richard.wc.tsang@gmail.com>
2014-12-16 13:06:25 -0500

    Initial javascript overlay on Connect program

---
+++ etcbc/js/main.js
@@ -0,0 +1,35 @@
+
+/* Is it efficient to have an overall full document listener? */
+
+var elShown; // Global value to check for toggle
+document.addEventListener('click', toggleOverlay, false);
+
+
+function toggleOverlay (e) {
+
+   // worthwhile to check for activated-overlay immediately?
+   var el = e.target;
+   /* Need to handle if the h2 element is clicked */
+   if ( el.tagName == "H2" ) {
+      el = el.parentNode;
+   }
+
+   var overlay  = el.querySelector(".activated-overlay");
+
+   // if elShown is defined because an overlay is active
+   // if we can't find an activated-overlay class, presumably because
+   // we're clicking on the overlay itself
+   if ( elShown && overlay == null ) {
+      elShown.querySelector(".activated-overlay").style.display = "none";
+      elShown = null;
+   }
+   else if ( !elShown && el ) {
+      
+      switch ( el.id ) {
+         case "connect": 
+            elShown = el;
+            overlay.style.display = "initial";
+            break;
+      }
+   }
+}
