439577db754b0994295be973c416ad56be68e187
Richard Tsang <richard.wc.tsang@gmail.com>
2014-12-23 17:31:45 -0500

    removal of fullscreen overlay on program content, slider used instead

--- etcbc/js/main.js
+++ etcbc/js/main.js
@@ -1,19 +1,25 @@
 $(document).ready( function() {
 
+    /*
+     * Activated-overlay is primarily for immediate announcements
+     * so it's inlined as display: block at ready and then disabled
+     * by click afterwards
+     */
+    $( '.activated-overlay' ).on('click tap',  function(e) {
+        e.stopPropagation();
+        $(this).fadeOut().css("display","none");
+    });
+
+    /*
+     * We're going to slide the information down instead of using
+     * activated-overlay.
+     */
     $( "#main-programs" ).on('click', '.program', function ( e ) {
 
         // Pre-toggle administrative tasks
         e.stopPropagation(); // stop bubbling dude
-        $('.activated-overlay').css("display", "none");
 
-        $(this).children('.activated-overlay').fadeToggle();
+        $(this).children('.program-content').slideToggle();
     });
-
-
-    $( '.activated-overlay' ).on('click tap',  function(e) {
-        e.stopPropagation();
-        $(this).fadeOut(300).css("display","none");
-    });
-
-}
+});
 
